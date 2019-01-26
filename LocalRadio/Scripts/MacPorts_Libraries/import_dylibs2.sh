bf325866ef6c068ee703cc00ca3823a183bb19bf
dsward2 <dsward@arkphone.com>
2019-01-25 19:23:18 -0600

    This commit adds the SoX source code as a git submodule under the main project.  Previously, sox was acquired from MacPorts.
    A file titled "sox git and build notes.txt" contains instructions for cloning and configuring sox.  The configuration omits most of the file formats that sox is capable of supporting, but includes support for macOS Core Audio.  This allows several superfluous dylibs to be removed from the application bundle.

--- LocalRadio/Scripts/MacPorts_Libraries/import_dylibs2.sh
+++ LocalRadio/Scripts/MacPorts_Libraries/import_dylibs2.sh
@@ -10,12 +10,10 @@
 
 # Modify MacPorts tools and libraries for embed in application bundle
 
-#https://github.com/auriamg/macdylibbundler
-#dylibbundler -b -x ./LocalRadio.app/Contents/MacOS/LocalRadio -d ./MacPorts_Libraries/ -p @executable_path/../Frameworks/
+# install mackylibbundler from https://github.com/auriamg/macdylibbundler
 
 echo BUILT_PRODUCTS_DIR = ${BUILT_PRODUCTS_DIR}
 
-#EXECFILE=/Users/dsward/Library/Developer/Xcode/DerivedData/LocalRadio-fypfuqvcbqkwibconllscqvxdnhj/Build/Products/Debug/LocalRadio.app/Contents/MacOS/LocalRadio
 EXECFILE=${BUILT_PRODUCTS_DIR}/${EXECUTABLE_PATH}
 echo EXECFILE = ${EXECFILE}
 
@@ -37,6 +35,16 @@ if [ -d "${MODIFIEDLIBPATH}" ]; then
   exit 0
 fi
 
+cd ${BUILT_PRODUCTS_DIR}
+
+if [ ! -d "${WORKLIBPATH}" ]; then
+  mkdir ${WORKLIBPATH}
+fi
+
+if [ ! -d "${MODIFIEDLIBPATH}" ]; then
+  mkdir ${MODIFIEDLIBPATH}
+fi
+
 #####################################################################
 
 # Important: In MacPorts, be sure in install icecast2, not icecast.
@@ -44,45 +52,25 @@ fi
 # Due to the naming conflict, only one version of icecast can be installed,
 # so be sure it is icecast2.
 
-#SOXPATH=${SRCROOT}/LocalRadio/MacPorts_Tools/sox
-SOXPATH=${BUILT_PRODUCTS_DIR}/sox
 ICECASTPATH=${BUILT_PRODUCTS_DIR}/icecast
 
+cp ${SRCROOT}/Icecast-Server/externals/Icecast-Server/src/icecast ${ICECASTPATH}
+
+SOXPATH=${BUILT_PRODUCTS_DIR}/sox
 echo SOXPATH = ${SOXPATH}
 
-cp /opt/local/bin/sox ${SOXPATH}
-cp ${SRCROOT}/Icecast-Server/externals/Icecast-Server/src/icecast ${ICECASTPATH}
+echo cp ${SRCROOT}/sox/externals/sox/src/.libs/sox ${SOXPATH}
+cp ${SRCROOT}/sox/externals/sox/src/.libs/sox ${SOXPATH}
+
+echo cp ${SRCROOT}/sox/externals/sox/src/.libs/libsox.3.dylib ${BUILT_PRODUCTS_DIR}/Libraries_Copy/libsox.3.dylib
+cp ${SRCROOT}/sox/externals/sox/src/.libs/libsox.3.dylib ${BUILT_PRODUCTS_DIR}/Libraries_Copy/libsox.3.dylib
 
 #####################################################################
 
 # fix sox library loading paths
 
-echo install_name_tool -change /opt/local/lib/libsox.3.dylib @executable_path/../Frameworks/libsox.3.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libsox.3.dylib @executable_path/../Frameworks/libsox.3.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libpng16.16.dylib @executable_path/../Frameworks/libpng16.16.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libpng16.16.dylib @executable_path/../Frameworks/libpng16.16.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libmagic.1.dylib @executable_path/../Frameworks/libmagic.1.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libmagic.1.dylib @executable_path/../Frameworks/libmagic.1.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libFLAC.8.dylib @executable_path/../Frameworks/libFLAC.8.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libFLAC.8.dylib @executable_path/../Frameworks/libFLAC.8.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libmad.0.dylib @executable_path/../Frameworks/libmad.0.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libmad.0.dylib @executable_path/../Frameworks/libmad.0.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libid3tag.0.dylib @executable_path/../Frameworks/libid3tag.0.3.0.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libid3tag.0.dylib @executable_path/../Frameworks/libid3tag.0.3.0.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libmp3lame.0.dylib @executable_path/../Frameworks/libmp3lame.0.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libmp3lame.0.dylib @executable_path/../Frameworks/libmp3lame.0.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libtwolame.0.dylib @executable_path/../Frameworks/libtwolame.0.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libtwolame.0.dylib @executable_path/../Frameworks/libtwolame.0.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libopusfile.0.dylib @executable_path/../Frameworks/libopusfile.0.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libopusfile.0.dylib @executable_path/../Frameworks/libopusfile.0.dylib ${SOXPATH}
+echo install_name_tool -change /usr/local/lib/libsox.3.dylib @executable_path/../Frameworks/libsox.3.dylib ${SOXPATH}
+install_name_tool -change /usr/local/lib/libsox.3.dylib @executable_path/../Frameworks/libsox.3.dylib ${SOXPATH}
 
 echo install_name_tool -change /opt/local/lib/libvorbisenc.2.dylib @executable_path/../Frameworks/libvorbisenc.2.dylib ${SOXPATH}
 install_name_tool -change /opt/local/lib/libvorbisenc.2.dylib @executable_path/../Frameworks/libvorbisenc.2.dylib ${SOXPATH}
@@ -96,28 +84,9 @@ install_name_tool -change /opt/local/lib/libvorbis.0.dylib @executable_path/../F
 echo install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib ${SOXPATH}
 install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib ${SOXPATH}
 
-echo install_name_tool -change /opt/local/lib/libwavpack.1.dylib @executable_path/../Frameworks/libwavpack.1.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libwavpack.1.dylib @executable_path/../Frameworks/libwavpack.1.dylib ${SOXPATH}
-
-echo install_name_tool -change /opt/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib ${SOXPATH}
-
-# here, we change from MacPorts libz to the macOS standard libz
-echo install_name_tool -change /opt/local/lib/libz.1.dylib /usr/lib/libz.1.dylib ${SOXPATH}
-install_name_tool -change /opt/local/lib/libz.1.dylib /usr/lib/libz.1.dylib ${SOXPATH}
-
 #####################################################################
 
 # fix icecast library loading paths
-
-echo install_name_tool -change /opt/local/lib/libidn2.0.dylib @executable_path/../Frameworks/libidn2.0.dylib ${ICECASTPATH}
-install_name_tool -change /opt/local/lib/libidn2.0.dylib @executable_path/../Frameworks/libidn2.0.dylib ${ICECASTPATH}
-
-echo install_name_tool -change /opt/local/lib/libpsl.5.dylib @executable_path/../Frameworks/libpsl.5.dylib ${ICECASTPATH}
-install_name_tool -change /opt/local/lib/libpsl.5.dylib @executable_path/../Frameworks/libpsl.5.dylib ${ICECASTPATH}
-
-echo install_name_tool -change /opt/local/lib/libspeex.1.dylib @executable_path/../Frameworks/libspeex.1.dylib ${ICECASTPATH}
-install_name_tool -change /opt/local/lib/libspeex.1.dylib @executable_path/../Frameworks/libspeex.1.dylib ${ICECASTPATH}
 
 echo install_name_tool -change /opt/local/lib/libtheora.0.dylib @executable_path/../Frameworks/libtheora.0.dylib ${ICECASTPATH}
 install_name_tool -change /opt/local/lib/libtheora.0.dylib @executable_path/../Frameworks/libtheora.0.dylib ${ICECASTPATH}
@@ -129,8 +98,6 @@ echo install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/..
 install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib ${ICECASTPATH}
 
 # here, we change from MacPorts libraries to the macOS standard libraries
-echo install_name_tool -change /opt/local/lib/libcurl.4.dylib /usr/lib/libcurl.dylib ${ICECASTPATH}
-install_name_tool -change /opt/local/lib/libcurl.4.dylib /usr/lib/libcurl.dylib ${ICECASTPATH}
 
 echo install_name_tool -change /opt/local/lib/libssl.1.0.0.dylib /usr/lib/libssl.dylib ${ICECASTPATH}
 install_name_tool -change /opt/local/lib/libssl.1.0.0.dylib /usr/lib/libssl.dylib ${ICECASTPATH}
@@ -144,46 +111,26 @@ install_name_tool -change /opt/local/lib/libxslt.1.dylib /usr/lib/libxslt.dylib 
 echo install_name_tool -change /opt/local/lib/libxml2.2.dylib /usr/lib/libxml2.dylib ${ICECASTPATH}
 install_name_tool -change /opt/local/lib/libxml2.2.dylib /usr/lib/libxml2.dylib ${ICECASTPATH}
 
-echo install_name_tool -change /opt/local/lib/libiconv.2.dylib /usr/lib/libiconv.dylib ${ICECASTPATH}
-install_name_tool -change /opt/local/lib/libiconv.2.dylib /usr/lib/libiconv.dylib ${ICECASTPATH}
-
 echo install_name_tool -change /opt/local/lib/libz.1.dylib /usr/lib/libz.dylib ${ICECASTPATH}
 install_name_tool -change /opt/local/lib/libz.1.dylib /usr/lib/libz.dylib ${ICECASTPATH}
 
 echo install_name_tool -change /opt/local/lib/liblzma.5.dylib /usr/lib/liblzma.dylib ${ICECASTPATH}
 install_name_tool -change /opt/local/lib/liblzma.5.dylib /usr/lib/liblzma.dylib ${ICECASTPATH}
 
-
 #####################################################################
-
 
 # Now fix the libraries copied from MacPorts to load other interdependent libraries from the app bundle instead
 
 cd ${BUILT_PRODUCTS_DIR}
 
-if [ ! -d "${WORKLIBPATH}" ]; then
-  mkdir ${WORKLIBPATH}
-fi
-
-if [ ! -d "${MODIFIEDLIBPATH}" ]; then
-  mkdir ${MODIFIEDLIBPATH}
-fi
-
 # Run dylibbundler against main app executable
-#echo dylibbundler -b -x "${EXECFILE}" -d "${LIBPATH}" -p "${NEWLOADERPATH}"
+
 echo /opt/local/bin/dylibbundler -b -x "${EXECFILE}" -d "${MODIFIEDLIBPATH}" -p "${NEWLOADERPATH}"
-#/opt/local/bin/dylibbundler -b -x "${EXECFILE}" -d "${MODIFIEDLIBPATH}" -p "${NEWLOADERPATH}"
-
-
 
 # list of dylibs in application bundle
 #TARGETS=(`ls "${BUNDLELIBPATH}" | grep dylib`)
 
-#TARGETS="libao.4.dylib libev.4.dylib libfftw3f.3.dylib libFLAC.8.dylib libiconv.2.dylib libid3tag.0.3.0.dylib libliquid.dylib liblzma.5.dylib libmad.0.dylib libmagic.1.dylib libmp3lame.0.dylib libncurses.6.dylib libogg.0.dylib libopencore-amrnb.0.dylib libopencore-amrwb.0.dylib libopus.0.dylib libopusfile.0.dylib libpng16.16.dylib libreadline.7.0.dylib libshout.3.dylib libsndfile.1.dylib libsox.3.dylib libspeex.1.dylib libsqlite3.0.dylib libtag_c.0.0.0.dylib libtag.1.17.0.dylib libtheora.0.dylib libtwolame.0.dylib libusb-1.0.0.dylib libvorbis.0.dylib libvorbisenc.2.dylib libvorbisfile.3.dylib libwavpack.1.dylib"
-
-
-TARGETS="libao.4.dylib libev.4.dylib libfftw3f.3.dylib libFLAC.8.dylib libid3tag.0.3.0.dylib libidn2.0.dylib libliquid.dylib libmad.0.dylib libmagic.1.dylib libmp3lame.0.dylib libogg.0.dylib libopus.0.dylib libopusfile.0.dylib libpng16.16.dylib libpsl.5.dylib libsndfile.1.dylib libsox.3.dylib libspeex.1.dylib libtheora.0.dylib libtwolame.0.dylib libusb-1.0.0.dylib libvorbis.0.dylib libvorbisenc.2.dylib libvorbisfile.3.dylib libwavpack.1.dylib"
-
+TARGETS="libfftw3f.3.dylib libFLAC.8.dylib libliquid.dylib libogg.0.dylib libopus.0.dylib libopusfile.0.dylib libsndfile.1.dylib libtheora.0.dylib libusb-1.0.0.dylib libvorbis.0.dylib libvorbisenc.2.dylib libvorbisfile.3.dylib"
 
 for TARGET in ${TARGETS[*]} ; do
 
